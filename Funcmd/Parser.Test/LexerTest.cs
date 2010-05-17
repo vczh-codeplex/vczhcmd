4782c92467b9c52238a696ed1c1c2b1bf885bc25
Zihan Chen <vczh@163.com>
2010-05-17 17:24:01 +0000

    Lexer and unit test

---
+++ Funcmd/Parser.Test/LexerTest.cs
@@ -0,0 +1,63 @@
+﻿using Funcmd.Parser;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace Parser.Test
+{
+    [TestClass()]
+    public class LexerTest
+    {
+        private enum TokenType
+        {
+            Number,
+            Add,
+            Mul,
+            Open,
+            Close
+        }
+
+        private Lexer<TokenType> lexer = null;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            lexer = new Lexer<TokenType>();
+            lexer.AddToken(@"\d+", TokenType.Number);
+            lexer.AddToken(@"\+|-", TokenType.Add);
+            lexer.AddToken(@"\*|/", TokenType.Mul);
+            lexer.AddToken(@"\(", TokenType.Open);
+            lexer.AddToken(@"\)", TokenType.Close);
+        }
+
+        [TestMethod]
+        public void TestSuccessfulInput()
+        {
+            Assert.IsTrue(
+                new TokenType[] {
+                    TokenType.Open, 
+                    TokenType.Number,
+                    TokenType.Add, 
+                    TokenType.Number,
+                    TokenType.Close, 
+                    TokenType.Mul,
+                    TokenType.Open,
+                    TokenType.Number,
+                    TokenType.Add,
+                    TokenType.Number,
+                    TokenType.Close
+                }
+                .SequenceEqual(lexer.Parse("(11+22)*(33+44)")
+                    .Select(t => t.Tag)
+                    )
+                );
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ParserException<TokenType>))]
+        public void TestFailInput()
+        {
+            lexer.Parse("(11+22) * (33+44)").ToArray();
+        }
+    }
+}
---
+++ Funcmd/Parser/Lexer.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Funcmd.Parser
+{
+    public class Lexer<T>
+    {
+        public class TokenValue
+        {
+            public int Position { get; internal set; }
+            public string Value { get; internal set; }
+        }
+
+        public class Token : TokenValue
+        {
+            public T Tag { get; internal set; }
+        }
+
+        private List<Regex> tokenRules = new List<Regex>();
+        private List<Func<TokenValue, T>> tokenHandlers = new List<Func<TokenValue, T>>();
+
+        public Lexer()
+        {
+            tokenRules.Add(null);
+            tokenHandlers.Add(null);
+        }
+
+        public void AddToken(string rule, Func<TokenValue, T> handler)
+        {
+            tokenRules.Add(new Regex(rule));
+            tokenHandlers.Add(handler);
+        }
+
+        public void AddToken(string rule, T tag)
+        {
+            tokenRules.Add(new Regex(rule));
+            tokenHandlers.Add(t => tag);
+        }
+
+        public IEnumerable<Token> Parse(string input)
+        {
+            int position = 0;
+            while (true)
+            {
+                var match = tokenRules
+                    .Select(r => r == null ? null : new { Source = r, Match = r.Match(input, position) })
+                    .Where(m => m == null || m.Match.Success && m.Match.Index == position)
+                    .Aggregate((a, b) =>
+                        a == null ? b :
+                        a.Match.Length >= b.Match.Length ? a :
+                        b);
+                if (match != null)
+                {
+                    Token token = new Token()
+                    {
+                        Position = position,
+                        Value = match.Match.Value
+                    };
+                    token.Tag = tokenHandlers[tokenRules.IndexOf(match.Source)](token);
+                    yield return token;
+                    position += match.Match.Length;
+                }
+                else if (position == input.Length)
+                {
+                    break;
+                }
+                else
+                {
+                    throw new ParserException<T>("遇到无法解析的字符串。", new Token()
+                    {
+                        Position = position
+                    });
+                }
+            }
+        }
+    }
+
+    public class ParserException<T> : Exception
+    {
+        public Lexer<T>.Token Token { get; private set; }
+
+        public ParserException(string message, Lexer<T>.Token token)
+            : base(message)
+        {
+            this.Token = token;
+        }
+    }
+}
