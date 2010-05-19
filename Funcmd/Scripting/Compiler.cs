574e7b554b83cfacf2d9c8e2664b15c1ee25bc57
vczh_cp <SND\vczh_cp>
2010-05-19 18:28:50 +0000

    Grammar

---
+++ Funcmd/Scripting/Compiler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Scripting
+{
+    /*
+     * 123
+     * "vczh"
+     * true
+     * false
+     * 'id
+     * id
+     * id?  //only for pattern variable
+     * [v1,v2,v3]
+     * v1:v2:v3
+     * func p1 p2
+     * case exp
+     *   pattern1->v1
+     *   pattern2->v2
+     * do
+     *   var=v1
+     *   v2
+     * (x,y)=>(x+y)
+     */
+    class Compiler
+    {
+    }
+}
