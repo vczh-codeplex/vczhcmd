7a1eb8199c66610b34b7ec7583c5b62e45747bcf
Zihan Chen <vczh@163.com>
2010-05-23 16:38:27 +0000

    Funcmd.Scripting Lexical Analyzer

--- Funcmd/Scripting/Compiler.cs
+++ Funcmd/Scripting/Compiler.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Funcmd.Parser;
 
-namespace Scripting
+namespace Funcmd.Scripting
 {
-    /*
+    /* [EXPRESSION]
      * 123
      * "vczh"
      * true
      * false
      * 'id
      * id
-     * id?  //only for pattern variable
      * [v1,v2,v3]
      * v1:v2:v3
      * func p1 p2
@@ -20,11 +20,52 @@ namespace Scripting
      *   pattern1->v1
      *   pattern2->v2
      * do
-     *   var=v1
-     *   v2
+     *   name patterns = expression
+     *   expression
      * (x,y)=>(x+y)
+     * [DECLARATION]
+     * name patterns = expression
      */
-    class Compiler
+
+    enum TokenType
     {
+        Integer,
+        Float,
+        String,
+        Flag,
+        Identifier,
+        OpenSquare,
+        CloseSquare,
+        OpenBracket,
+        CloseBracket,
+        Colon,
+        Comma,
+        Case,
+        Lambda,
+        Equal,
+    }
+
+    class Compiler : LexerParserBase<TokenType, Program>
+    {
+        protected override void Initialize(out Lexer<TokenType> lexer, out IParser<Lexer<TokenType>.Token, Program, object> parser)
+        {
+            lexer = new Lexer<TokenType>();
+            lexer.AddToken(@"\d+\.\d+", TokenType.Float);
+            lexer.AddToken(@"\d+", TokenType.Integer);
+            lexer.AddToken(@"""([^""]|\.)*""", TokenType.String);
+            lexer.AddToken(@"'[a-zA-Z_]\w*", TokenType.Flag);
+            lexer.AddToken(@"[a-zA-Z_]\w*", TokenType.Identifier);
+            lexer.AddToken(@"\(", TokenType.OpenBracket);
+            lexer.AddToken(@"\)", TokenType.CloseBracket);
+            lexer.AddToken(@"\[", TokenType.OpenSquare);
+            lexer.AddToken(@"\]", TokenType.CloseSquare);
+            lexer.AddToken(@":", TokenType.Colon);
+            lexer.AddToken(@",", TokenType.Comma);
+            lexer.AddToken(@"->", TokenType.Case);
+            lexer.AddToken(@"=>", TokenType.Lambda);
+            lexer.AddToken(@"=", TokenType.Equal);
+
+            parser = null;
+        }
     }
 }
---
+++ Funcmd/Scripting/Expression.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Funcmd.Scripting
+{
+    abstract class Expression
+    {
+    }
+
+    abstract class Declaration
+    {
+    }
+
+    class Program
+    {
+    }
+}
