2e1d75629004cb69f19f493c1298e85102e72db7
Zihan Chen <vczh@163.com>
2010-06-05 17:55:12 +0000

    ICalendar

---
+++ Funcmd/Funcmd/ColorSettings/ICalendar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Xml.Linq;
+using System.Windows.Forms;
+
+namespace Funcmd.ColorSettings
+{
+    public interface ICalendar
+    {
+        Size CalendarSize { get; }
+        void Draw(Graphics graphics, Point location, Point cursor);
+        Cursor GetCursor(Point location, Point cursor);
+        void Click(Point location, Point cursor);
+    }
+}
