5932f0ba729da4c2e239f11d388a16d753f460b4
Zihan Chen <vczh@163.com>
2010-06-06 03:39:30 +0000

    Calendar interface finished

--- Funcmd/Funcmd/Calendar/ICalendar.cs
+++ Funcmd/Funcmd/Calendar/ICalendar.cs
@@ -6,13 +6,25 @@ using System.Drawing;
 using System.Xml.Linq;
 using System.Windows.Forms;
 
-namespace Funcmd.ColorSettings
+namespace Funcmd.Calendar
 {
     public interface ICalendar
     {
         Size CalendarSize { get; }
+        DateTime CurrentDay { get; set; }
+
         void Draw(Graphics graphics, Point location, Point cursor);
         Cursor GetCursor(Point location, Point cursor);
         void Click(Point location, Point cursor);
+
+        event CalendarDaySelectedHandler CalendarDaySelected;
+        event EventHandler CurrentDayChanged;
     }
+
+    public class CalendarDaySelectedEventArgs
+    {
+        public DateTime Day { get; set; }
+    }
+
+    public delegate void CalendarDaySelectedHandler(object sender,CalendarDaySelectedEventArgs e);
 }
---
+++ Funcmd/Funcmd/Calendar/MonthCalendar.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Funcmd.Calendar
+{
+    public class MonthCalendar : ICalendar
+    {
+        public Size CalendarSize
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public DateTime CurrentDay
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Draw(Graphics graphics, Point location, Point cursor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cursor GetCursor(Point location, Point cursor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Click(Point location, Point cursor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event CalendarDaySelectedHandler CalendarDaySelected;
+
+        public event EventHandler CurrentDayChanged;
+    }
+}
