1da880f4b5a17af9bfd5f7509446fabf52792ac6
Zihan Chen <vczh@163.com>
2010-06-06 08:52:15 +0000

    MonthCalendar

--- Funcmd/Funcmd/Calendar/ICalendar.cs
+++ Funcmd/Funcmd/Calendar/ICalendar.cs
@@ -12,18 +12,26 @@ namespace Funcmd.Calendar
     {
         Size CalendarSize { get; }
         DateTime CurrentDay { get; set; }
+        ICalendarPainter Painter { get; set; }
 
         void Draw(Graphics graphics, Point location, Point cursor);
         Cursor GetCursor(Point location, Point cursor);
-        void Click(Point location, Point cursor);
+        void MouseMove(Point location, Point cursor);
+        void MouseClick(Point location, Point cursor);
 
+        event CalendarDaySelectedHandler CalendarDayEntered;
         event CalendarDaySelectedHandler CalendarDaySelected;
         event EventHandler CurrentDayChanged;
     }
 
     public class CalendarDaySelectedEventArgs
     {
-        public DateTime Day { get; set; }
+        public DateTime Day { get; private set; }
+
+        public CalendarDaySelectedEventArgs(DateTime day)
+        {
+            Day = day;
+        }
     }
 
     public delegate void CalendarDaySelectedHandler(object sender,CalendarDaySelectedEventArgs e);
