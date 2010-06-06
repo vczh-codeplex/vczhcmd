3b3b3d98005c354e624045e9018bf8cafe2f65f7
vczh_cp <SND\vczh_cp>
2010-06-06 13:21:23 +0000

    Expose State Monad API, not tested yet

--- Funcmd/Scripting/ScriptingLibrary.cs
+++ Funcmd/Scripting/ScriptingLibrary.cs
@@ -12,11 +12,17 @@ namespace Funcmd.Scripting
             e.DefineValue("pure", ScriptingValue.CreateValue(new PureMonad()));
             e.DefineValue("ordered", ScriptingValue.CreateValue(new OrderedMonad()));
             e.DefineValue("state", ScriptingValue.CreateFunction(State, 1));
+            e.DefineValue("continue", ScriptingValue.CreateFunction(Continue, 1));
         }
 
-        public static ScriptingValue State(ScriptingValue[] arguments)
+        private static ScriptingValue State(ScriptingValue[] arguments)
         {
             return ScriptingValue.CreateValue(new StateMonad(arguments[0].ValueWrapper));
+        }
+
+        private static ScriptingValue Continue(ScriptingValue[] arguments)
+        {
+            return ScriptingValue.CreateValue(true);
         }
     }
 }
