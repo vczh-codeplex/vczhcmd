61e19419aa75ff79f63651b926ab206f65001f63
vczh_cp <SND\vczh_cp>
2010-06-29 13:33:54 +0000

    ICalendarTimer

---
+++ Funcmd/Funcmd/CalendarTimer/EventTimerType.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Funcmd.CalendarTimer
+{
+    public class EventTimerType : IObjectEditorType
+    {
+        private CalendarTimerProvider provider;
+
+        public EventTimerType(CalendarTimerProvider provider)
+        {
+            this.provider = provider;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "普通事件";
+            }
+        }
+
+        public IObjectEditorObject CreateObject()
+        {
+            return new EventTimer(provider);
+        }
+
+        public System.Windows.Forms.Control EditObject(IObjectEditorObject obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Funcmd/Funcmd/CalendarTimer/EveryDayTimerType.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Funcmd.CalendarTimer
+{
+    public class EveryDayTimerType : IObjectEditorType
+    {
+        private CalendarTimerProvider provider;
+
+        public EveryDayTimerType(CalendarTimerProvider provider)
+        {
+            this.provider = provider;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "定时事件";
+            }
+        }
+
+        public IObjectEditorObject CreateObject()
+        {
+            return new EveryDayTimer(provider);
+        }
+
+        public System.Windows.Forms.Control EditObject(IObjectEditorObject obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
